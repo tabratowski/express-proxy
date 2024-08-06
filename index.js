148ef70539d4573e1b0aa643bab3fe291331f083
Tomasz Abratowski <tomasz.abratowski@wpengine.com>
2024-08-06 11:33:15 +0200

    logs

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ const port = 3000;
 
 app.get('/', (_req, res) => {
   res.send('Hello in Express World!');
+  console.error("some error 122###############");
+  process.stdout.write("FIND-IT-TEST process stdout")
+  process.stderr.write("FIND-IT-TEST process error")
+  console.log("FIND-IT-TEST console log")
+  console.error("FIND-IT-TEST console error")
 });
 
 app.use('/proxy', proxy);
